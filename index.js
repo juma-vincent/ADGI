c4f9038f8e2fbc915af878dcd22f92471de991e0
juma-vincent <vinsoncode@gmail.com>
2022-02-04 16:32:30 +0300

    renewed the project

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('Welcome to Siaya county');
+    res.send('Hi Winnie, welcome to GIMORES');
 })
 
 // if(process.env.NODE_ENV === 'production'){
