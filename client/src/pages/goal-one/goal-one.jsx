5a5d1c19efa05b71d17a59310bda38f51a2540ec
juma-vincent <vinsoncode@gmail.com>
2024-04-07 22:12:35 +0300

    updated goal-one.jsx and .scss

--- client/src/pages/goal-one/goal-one.jsx
+++ client/src/pages/goal-one/goal-one.jsx
@@ -90,8 +90,8 @@ const GoalOne = () =>{
                 </div>
 
                 <div className='goal-one-economic-impact'>   
-                    <div style={{width: '90%', margin:'0 auto', padding:'3%', backgroundColor:'rgba(224, 247, 255, 0.6)', 
-                                borderRadius:'10px', marginTop:'-8%'}}>                  
+                    <div style={{width: '90%', margin:'0 auto', padding:'3%', backgroundColor:'rgba(232, 249, 255, 0.993)', 
+                                borderRadius:'20px', marginTop:'-8%'}}>                  
 
                         <h2 style={{textAlign: 'center'}}>Impact on Economic Growth: </h2>
 
