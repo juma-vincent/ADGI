f73b423654e2dbee55ab1559d646844642023aad
juma-vincent <vinsoncode@gmail.com>
2024-04-12 06:19:39 +0300

    modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,9 @@ import DonationPage from './pages/donation-page/donation-page';
 import ContactSuccessModal from './components/modals/contact-success-modal/contact-success-modal';
 import DonationSuccessModal from './components/modals/donation-success-modal/donation-success-modal';
 import GoalOne from './pages/goal-one/goal-one';
+import GoalTwo from './pages/goal-two/goal-two';
+import GoalThree from './pages/goal-three/goal-three';
+import GoalFour from './pages/goal-four/goal-four';
 
 
 
@@ -20,6 +23,9 @@ function App() {
         <Routes>
           <Route path='/' element={ <Homepage/>} />
           <Route path='/goal-one' element={<GoalOne />}/>
+          <Route path='/goal-two' element={<GoalTwo />}/>
+          <Route path='/goal-three' element={<GoalThree />}/>
+          <Route path='/goal-four' element={<GoalFour />}/>
           {/* <Route path='/donation' element={<DonationPage />}/> */}
         </Routes>
         <Footer/>
